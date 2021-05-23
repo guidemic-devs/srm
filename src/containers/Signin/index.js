0457220ef2f73f7f49c0e1807d46328db9e5cab3
RameshOj <84656298+RameshOj@users.noreply.github.com>
2021-05-23 18:13:12 +0530

    New Files 23/05/21

--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from "../../components/Layout";
 import {Container, Form, Row, Col, Button} from "react-bootstrap";
 import Input from '../../components/UI/Input';
+import "../../App.css";
 
 /**
 * @author
@@ -11,10 +12,13 @@ import Input from '../../components/UI/Input';
 const Signin = (props) => {
   return(
    <Layout>
-     <Container>
-       <Row style={{marginTop:"40px"}}>
-         <Col md={{span:6, offset:3}}>
+     <Container><center><h1 className = "Heading">Sign In to SRM</h1></center> 
+     
+       <Row className="justify-content-md-center" style={{marginTop:"40px"}}>
+         <Col md={{span:4, offset:0}}>
+         <div className = "box">
          <Form>
+         
          <Input 
             label="Email"
             placeholder = "Email"
@@ -30,13 +34,24 @@ const Signin = (props) => {
             type="password"
             onChange={()=>{}}
             />
-  <Button variant="primary" type="submit">
+           <div className="forgotPassword"> <a href="">Forgot Password</a><br></br></div>
+           
+  <Button styles={{margin:"auto"}} variant="primary" type="submit" size="lg" block>
     Submit
   </Button>
+  
 </Form>
+</div>
+<center>
+         <br />
+       <div className="newBox">
+        <p>New to SRM? <a href="">Create an Account</a></p>
+      </div>
+       </center>
          </Col>
        </Row>
-     
+       
+      
      </Container>
    </Layout>
    
